64c57bcb050114dbb7e7f8a95af78fe8a0a95017
DESKTOP-STDIL6J\Gavin <852905071@qq.com>
2017-05-08 23:41:21 +0800

    第一问可完成 第二问待完成

--- main.cpp
+++ main.cpp
@@ -5,14 +5,20 @@
 #include<map>
 #include<vector>
 #include<algorithm>
-#include"std_lib_facilities.h"
 using namespace std;
-
+string getKey(map<string, int>&m,int value);
+string getKey(map<string, int>&m, int value) {
+	map<string, int>::iterator it = m.begin();
+	for (;it != m.end();++it) {
+		if (it->second == value)
+			return it->first;
+	}
+	return "";
+}
 int main() {
 	set<string>lexicon;
 	ifstream infile;
 	infile.open("dictionary.txt");
-	if (infile.fail()) error("Can't open EnglishWords.txt");
 	while (true) {
 		string word;
 		getline(infile, word);
@@ -20,11 +26,11 @@ int main() {
 		lexicon.insert(word);
 	}
 	int n;
-	while (cout << "please give a number:", cin>>n) {
+	cout << "please give a number:";
+	cin >> n;
 	map<string, int>freq;
-	map<string, int>::iterator it = freq.begin();
 	string word;
-	for (string word:lexicon) {
+	for (string word : lexicon) {
 		if (word.size() < n) {
 			continue;
 		}
@@ -33,17 +39,42 @@ int main() {
 			freq[suffix] ++;
 		}
 	}
+	cout << freq["ing"] << endl;
 	vector<int>value;
+	map<string, int>::iterator it = freq.begin();
 	for (;it != freq.end();++it) {
 		value.push_back(it->second);
 	}
 	sort(value.begin(), value.end());
 	for (int i = 0;i < 10;i++) {
-		cout << "suffix:";
-		cout << value[value.size() - i - 1];
-	}
+		int n = value[value.size() - i - 1];
+		cout << "suffix:"<<getKey(freq,n)<<"\t";
+		cout << n<<endl;
 	}
 
+	cout << "what suffix do you want enter:";
+	string SUFFIX;
+	cin >> SUFFIX;
+	int count=0;
+	map<string, int>length;
+	for (string word : lexicon) {
+		if (word.size() < SUFFIX.size()) {
+			continue;
+		}
+		else {
+			string suffix = word.substr(word.size() - SUFFIX.size());
+			if (suffix == SUFFIX)
+				count++;
+			length[word] = word.size();
+		}
+	}
+	cout << count<<endl;
+	vector<int>len;
+	map<string, int>::iterator IT = length.begin();
+	for (;IT != length.end();++IT) {
+		len.push_back(IT->second);
+	}
+	sort(len.begin(), len.end());
 	infile.close();
 	system("pause");
 	return 0;
